18ad1baadbbbabf007e09f7117e2f1ed7fb976c6
Joseph Ayitey <joseph.yaw.ayitey@gmail.com>
2026-03-10 16:14:22 +0100

    add formatTime and createSection helper functions

--- script.js
+++ script.js
@@ -4,8 +4,44 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { countUsers } from "./common.mjs";
+import { getUserIDs } from "./data.js"
+import { countUsers,  analyzeUser } from "./data-service.js";
 
 window.onload = function () {
   document.querySelector("body").innerText = `There are ${countUsers()} users`;
 };
+
+function formatTime(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}m ${secs}s`;
+}
+
+function createSection(title, content) {
+    const section = document.createElement('section');
+    section.className = 'result-section';
+    
+    const heading = document.createElement('h2');
+    heading.textContent = title;
+    section.appendChild(heading);
+    
+    if (typeof content === 'string') {
+        const p = document.createElement('p');
+        p.textContent = content;
+        section.appendChild(p);
+    } else if (Array.isArray(content)) {
+        const ul = document.createElement('ul');
+        content.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = item;
+            ul.appendChild(li);
+        });
+        section.appendChild(ul);
+    } else {
+        const p = document.createElement('p');
+        p.textContent = content;
+        section.appendChild(p);
+    }
+    
+    return section;
+}
